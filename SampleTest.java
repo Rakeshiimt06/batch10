4f68b00d7a1ea3c9867df404e081acc3e16e3212
Rakeshiimt06 <rakeshiimt06@gmail.com>
2021-06-01 17:38:05 +0530

    Create SampleTest.java

---
+++ SampleTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SampleTest {
+  
+  @Test
+  public void bootcampTest(){
+    int a = 1;
+    int b = 2;
+    assertTrue(a + b== 3);
+   }
+}
